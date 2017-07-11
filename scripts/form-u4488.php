c83f858ac6643ea00374792c8e8f3f2b28b5311f
mcolors <airminorcorp@gmail.com>
2017-07-12 02:06:19 +0300

    1

--- scripts/form-u4488.php
+++ scripts/form-u4488.php
@@ -34,24 +34,23 @@ $form = array(
 		'custom_U4496' => array(
 			'order' => 1,
 			'type' => 'string',
-			'label' => 'Имя:',
+			'label' => 'Пользовательский текст:',
 			'required' => true,
 			'errors' => array(
-				'required' => 'Поле \'Имя:\' не может быть пустым.'
+				'required' => 'Поле \'Пользовательский текст:\' не может быть пустым.'
 			)
 		),
 		'custom_U9036' => array(
 			'order' => 2,
 			'type' => 'string',
-			'label' => 'Номер телефона:',
+			'label' => 'Пользовательский текст:',
 			'required' => true,
 			'errors' => array(
-				'required' => 'Поле \'Номер телефона:\' не может быть пустым.'
+				'required' => 'Поле \'Пользовательский текст:\' не может быть пустым.'
 			)
 		)
 	)
 );
 
 process_form($form);
-echo "<script>parent.location='../upsell.php?name=".$_POST['custom_U4496']."&phone=".$_POST['custom_U9036']."'</script>";
 ?>
